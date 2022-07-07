5e5cac470b1a4953b3d69a27e88523be480e775d
Tony Spegel <TonySpegel@googlemail.com>
2022-07-08 01:23:00 +0200

    add 'blog' to filter, change let to const

--- .eleventy.js
+++ .eleventy.js
@@ -61,14 +61,14 @@ module.exports = function(eleventyConfig) {
   });
 
   function filterTagList(tags) {
-    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts", "blog"].indexOf(tag) === -1);
   }
 
   eleventyConfig.addFilter("filterTagList", filterTagList)
 
   // Create an array of all tags
   eleventyConfig.addCollection("tagList", function(collection) {
-    let tagSet = new Set();
+    const tagSet = new Set();
     collection.getAll().forEach(item => {
       (item.data.tags || []).forEach(tag => tagSet.add(tag));
     });
