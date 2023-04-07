15dbc14d7e05da9fd6c70f8146a20071166a5d4b
Tony Spegel <TonySpegel@googlemail.com>
2023-04-07 22:42:17 +0200

    config for bundling

--- rollup.config.live.js
+++ rollup.config.live.js
@@ -1,52 +1,76 @@
 /**
- * Copyright © 2022 Tony Spegel
+ * Copyright © 2023 Tony Spegel
  */
 
-import summary from 'rollup-plugin-summary';
+import { generateSW } from 'rollup-plugin-workbox';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
+import template from 'rollup-plugin-html-literals';
+import summary from 'rollup-plugin-summary';
+import path from 'path';
+
+const prodDir = '_prod';
+
+const terse = () =>
+  terser({
+    ecma: 2020,
+    module: true,
+    warnings: true,
+    format: {
+      quote_style: 1,
+    },
+  });
+
+const outputSettings = {
+  dir: `${prodDir}/js/`,
+  format: 'es',
+  sourcemap: true,
+};
+
 export default [
   {
-    input: '_site/js/post.js',
-    output: {
-      dir: '_site/js/',
-      format: 'es',
-      sourcemap: true,
-    },
-    plugins: [
-      // Resolve imports from NPM
-      nodeResolve(),
-      // Minify JS
-      terser({
-        ecma: 2020,
-        module: true,
-        warnings: true,
-        format: {
-          quote_style: 1,
-        },
-      }),
-      summary(),
-    ],
+    input: `${prodDir}/js/image-comparison.js`,
+    output: outputSettings,
+    /**
+     * template() is missing for image-comparison,
+     * because there's a bug for units used in inline-styles.
+     * Needs to be fixed in the component itself.
+     */
+    plugins: [nodeResolve(), terse(), summary()],
   },
   {
-    input: '_site/js/index.js',
-    output: {
-      dir: '_site/js/',
-      format: 'es',
-      sourcemap: true,
-    },
+    input: `${prodDir}/js/theme-switch.js`,
+    output: outputSettings,
+    plugins: [nodeResolve(), template(), terse(), summary()],
+  },
+  {
+    input: `${prodDir}/js/post.js`,
+    output: outputSettings,
+    plugins: [nodeResolve(), template(), terse(), summary()],
+  },
+  {
+    input: `${prodDir}/js/index.js`,
+    output: outputSettings,
+    preserveEntrySignatures: 'strict',
     plugins: [
-      // Resolve imports from NPM
       nodeResolve(),
       // Minify JS
-      terser({
-        ecma: 2020,
-        module: true,
-        warnings: true,
-        format: {
-          quote_style: 1,
-        },
+      terse(),
+      generateSW({
+        globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
+        navigateFallback: '/index.html',
+        // where to output the generated sw
+        swDest: path.join(prodDir, 'sw.js'),
+        // directory to match patterns against to be precached
+        globDirectory: path.join(prodDir),
+        // cache any html js and css by default
+        globPatterns: ['**/*.{html,js,mjs,css,png,svg,woff2,webmanifest}'],
+        skipWaiting: true,
+        clientsClaim: true,
+        runtimeCaching: [
+          { urlPattern: 'polyfills/*.js', handler: 'CacheFirst' },
+        ],
       }),
       summary(),
     ],
